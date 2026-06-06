4ba7ca467130fc67c9db9382f2aedf284a393b48
SilvaOrdep <pedro.o.silva975@gmail.com>
2026-06-06 00:05:39 -0300

    docs: configura OpenAPI definition title

--- src/main/java/com/ucsal/aspmanager/microservice_espaco/MicroserviceEspacoApplication.java
+++ src/main/java/com/ucsal/aspmanager/microservice_espaco/MicroserviceEspacoApplication.java
@@ -1,10 +1,13 @@
 package com.ucsal.aspmanager.microservice_espaco;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "ASPManager API - Espaços", version = "1.0", description = "Microserviço de Gestão e Solicitação de Espaços Físicos"))
 @EnableDiscoveryClient
 public class MicroserviceEspacoApplication {
 
